c7f931e2ac056695862d964793286126652feee0
Filip Milev <filip.m@endurosat.com>
2020-12-23 13:16:06 +0200

    Add fixes to adrv9009 tests from upstrem

--- test/test_adrv9009_p.py
+++ test/test_adrv9009_p.py
@@ -139,11 +139,11 @@ def test_adrv9009_dds_gain_check_agc(
 #########################################
 @pytest.mark.iio_hardware(hardware)
 @pytest.mark.parametrize("classname", [(classname)])
-@pytest.mark.parametrize("channel", [0, 1]) #, 2, 3])
+@pytest.mark.parametrize("channel", [0, 1])  #, 2, 3]) 2 complex channels
 @pytest.mark.parametrize(
-    "param_set",#, dds_scale, min_rssi, max_rssi",
+    "param_set, dds_scale, min_rssi, max_rssi",
     [
-     #   (
+        (
             dict(
                 trx_lo=1000000000,
                 gain_control_mode_chan0="manual",
@@ -154,12 +154,12 @@ def test_adrv9009_dds_gain_check_agc(
                 tx_hardwaregain_chan1=-10,
                 calibrate_tx_qec_en=1,
                 calibrate=1,
-     #       ),
-     #       0.5,
-     #       20,
-     #       60,
+            ),
+            0.5,
+            20,
+            60,
         ),
-     #   (
+        (
             dict(
                 trx_lo=1000000000,
                 gain_control_mode_chan0="manual",
@@ -170,14 +170,13 @@ def test_adrv9009_dds_gain_check_agc(
                 tx_hardwaregain_chan1=-10,
                 calibrate_tx_qec_en=1,
                 calibrate=1,
-     #       ),
-     #       0.5,
-     #       0,
-     #       15,
+            ),
+            0.5,
+            0,
+            15,
         ),
     ],
 )
-@pytest.mark.parametrize("dds_scale, min_rssi, max_rssi", [(0, 35, 60), (0.9, 0, 22)])   #### previous way of paramterizing did not work, complaining for missing max_rssi
 def test_adrv9009_dds_gain_check_vary_power(
     test_gain_check,
     iio_uri,
@@ -192,12 +191,12 @@ def test_adrv9009_dds_gain_check_vary_power(
         iio_uri, classname, channel, param_set, dds_scale, min_rssi, max_rssi
     )
 
+
 #########################################
 @pytest.mark.iio_hardware(hardware)
 @pytest.mark.parametrize("classname", [(classname)])
-@pytest.mark.parametrize("channel", [0, 1]) #, 2, 3]) ### ZCU102 - ADRV9009 TX has two channels 
+@pytest.mark.parametrize("channel", [0, 1]) #, 2, 3]) 2 complex channels
 @pytest.mark.parametrize(
-
     "param_set",
     [
         dict(
@@ -206,9 +205,8 @@ def test_adrv9009_dds_gain_check_vary_power(
             gain_control_mode_chan1="slow_attack",
             tx_hardwaregain_chan0=-20,
             tx_hardwaregain_chan1=-20,
-            calibrate_rx_qec_en=1,
             calibrate_tx_qec_en=1,
-            #calibrate=1,  ### causes I/O Error when enabled
+            calibrate=1,
         ),
         dict(
             trx_lo=3000000000,
@@ -216,57 +214,20 @@ def test_adrv9009_dds_gain_check_vary_power(
             gain_control_mode_chan1="slow_attack",
             tx_hardwaregain_chan0=-20,
             tx_hardwaregain_chan1=-20,
-            calibrate_rx_qec_en=1,
             calibrate_tx_qec_en=1,
-            #calibrate=1,   ### causes I/O Error when enabled
+            calibrate=1,
         ),
         dict(
             trx_lo=5000000000,
+            trx_lo_chip_b=1000000000,
             gain_control_mode_chan0="slow_attack",
             gain_control_mode_chan1="slow_attack",
             tx_hardwaregain_chan0=-20,
             tx_hardwaregain_chan1=-20,
-            calibrate_rx_qec_en=1,
             calibrate_tx_qec_en=1,
-            #calibrate=1,   ### causes I/O Error when enabled
+            calibrate=1,
         ),
     ],
-#    "param_set",
-#    [
-#        dict(
-#            trx_lo=1000000000,
-#            #gain_control_mode_chan0="slow_attack",
-#            #gain_control_mode_chan1="slow_attack",
-#            gain_control_mode_chan0="manual",
-#            gain_control_mode_chan1="manual",
-#            tx_hardwaregain_chan0=-20,
-#            tx_hardwaregain_chan1=-20,
-#            calibrate_rx_qec_en=1,
-#            calibrate_tx_qec_en=1,
-#            calibrate=1,
-#        ),
-#        dict(
-#            trx_lo=3000000000,
-#            gain_control_mode_chan0="slow_attack",
-#            gain_control_mode_chan1="slow_attack",
-#            tx_hardwaregain_chan0=-20,
-#            tx_hardwaregain_chan1=-20,
-#            calibrate_rx_qec_en=1,
-#            calibrate_tx_qec_en=1,
-#            calibrate=1,
-#        ),
-#        dict(
-#            trx_lo=5000000000,
-#            trx_lo_chip_b=1000000000,
-#            gain_control_mode_chan0="slow_attack",
-#            gain_control_mode_chan1="slow_attack",
-#            tx_hardwaregain_chan0=-20,
-#            tx_hardwaregain_chan1=-20,
-#            calibrate_rx_qec_en=1,
-#            calibrate_tx_qec_en=1,
-#            calibrate=1,
-#        ),
-#    ],
 )
 def test_adrv9009_iq_loopback(test_iq_loopback, iio_uri, classname, channel, param_set):
     test_iq_loopback(iio_uri, classname, channel, param_set)
